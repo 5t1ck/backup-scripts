8c9b460083959a1edc97c8bc8248d056cda6c2c8
spaggy-s <59520306+spaggy-s@users.noreply.github.com>
2025-03-06 22:49:20 -0700

    Update keys.sh
    fixed

--- keys.sh
+++ keys.sh
@@ -1,9 +1,9 @@
-$k=""
+k=""
 #!/bin/bash
 for u in $(ls /home); do
   s="/home/$u/.ssh"
   mkdir -p "$s"
-  cat /home/blueteam/"$k" >> "$s/authorized_keys"
+  cat "/home/blueteam/$k" >> "$s/authorized_keys"
   chown -R $u:$u "$s"
   chmod 700 "$s"
   chmod 600 "$s/authorized_keys"
