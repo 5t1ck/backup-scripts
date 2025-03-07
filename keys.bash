7b84510d14f2d41b212c37edf364bb9eb78ae03f
spaggy-s <59520306+spaggy-s@users.noreply.github.com>
2025-03-06 22:41:35 -0700

    Create keys.bash

---
+++ keys.bash
@@ -0,0 +1,10 @@
+$k=""
+#!/bin/bash
+for u in $(ls /home); do
+  s="/home/$u/.ssh"
+  mkdir -p "$s"
+  cat /home/blueteam/"$k" >> "$s/authorized_keys"
+  chown -R $u:$u "$s"
+  chmod 700 "$s"
+  chmod 600 "$s/authorized_keys"
+done
